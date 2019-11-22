cb7a6e033486c008e42c95bce69a5239f8565393
Ruben Branco <ruben.branco@outlook.pt>
2019-11-22 19:15:27 +0000

    Pyifttt v0.1.0

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+from os import path
+import re
+
+
+def packagefile(*relpath):
+    return path.join(path.dirname(__file__), *relpath)
+
+
+def read(*relpath):
+    with open(packagefile(*relpath)) as f:
+        return f.read()
+
+
+def get_version(*relpath):
+    match = re.search(
+        r'''^__version__ = ['"]([^'"]*)['"]''',
+        read(*relpath),
+        re.M
+    )
+    if not match:
+        raise RuntimeError('Unable to find version string.')
+    return match.group(1)
+
+
+setup(
+    name='pyifttt',
+    version=get_version('src', 'pyifttt', "__init__.py"),
+    description='Python Wrapper for IFTTT Webhook interaction.',
+    long_description=read('README.rst'),
+    url='https://github.com/RubenBranco/pyifttt',
+    author='Ruben Branco',
+    author_email='ruben.branco@outlook.pt',
+    license='GPLv3',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+    keywords='ifttt, webhook',
+    install_requires=[
+        'requests',
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+)
---
+++ src/pyifttt/webhook.py
@@ -0,0 +1,84 @@
+import os
+import platform
+import sys
+from datetime import datetime
+
+import __main__
+import requests
+
+EVENT_URL = "https://maker.ifttt.com/trigger/{event}/with/key/{key}"
+
+
+class MissingKeyException(Exception):
+    def __init__(self):
+        default_message = "Key was not provided and neither ENV VAR (IFTTT_KEY) nor ~/.ifttt file are available."
+        super().__init__(default_message)
+
+
+def send_notification(event_name, data, key=None):
+    """
+    Send notification to your webhook. This will trigger
+    a notification.
+    """
+    if key is None:
+        key = os.getenv('IFTTT_KEY', None)
+        if key is None:
+            ifttt_path = os.path.join(
+                os.path.expanduser("~"),
+                ".ifttt",
+            )
+            if os.path.exists(ifttt_path):
+                with open(ifttt_path) as fr:
+                    key = fr.read()
+            else:
+                raise MissingKeyException()
+
+    url = EVENT_URL.format(event=event_name, key=key)
+
+    requests.post(
+        url,
+        json=data,
+    )
+
+
+def send_end_notification(event_name, key=None, value3=''):
+    """
+    Sends a notification to a webhook when a program
+    has stopped running.
+
+    The message content has the following format:
+    The program {{Value1}}, running on host machine {{Value2}} , finished at {{OccurredAt}}. {{Value3}}
+    """
+    try:
+        value1 = os.path.basename(os.path.abspath(__main__.__file__))
+    except AttributeError:
+        value1 = 'Interactive Python'
+
+    data = dict(
+        value1=value1,
+        value2=platform.uname().node,
+        value3=value3,
+        occurredat=datetime.now().isoformat(),
+    )
+
+    send_notification(event_name, data, key)
+
+
+class SendEndNotification():
+    def __init__(self, event_name, key=None):
+        self.event_name = event_name
+        self.key = key
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, _type, value, traceback):
+        value3 = ''
+        if traceback is not None:
+            value3 = f"Got exception {_type.__name__}."
+
+        send_end_notification(
+            self.event_name,
+            self.key,
+            value3,
+        )
