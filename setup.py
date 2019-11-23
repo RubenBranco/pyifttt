808a4de6adbf66d02222a0c6968b6de055ff6d41
Ruben Branco <ruben.branco@outlook.pt>
2019-11-23 13:02:07 +0000

    Fixing license classifier.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
     ],
     keywords='ifttt, webhook',
